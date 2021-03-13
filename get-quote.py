7e36bf48d84ec161f903e76d570c1c952f5a3429
Sandeep Kumar <80529777+SandeepMDJ007@users.noreply.github.com>
2021-03-13 22:38:48 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+import random
 def funt():
 #   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  last = 13
+  rnd = random.randint(0,last)
 
-  print(quotes[-1])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   funt()
